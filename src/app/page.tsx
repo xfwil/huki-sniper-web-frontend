6926f83b1d426908b59489d7b7e397e52f06ec68
tokinaa <tokinaa@sikuder.dev>
2023-08-14 05:50:30 +0700

    fix: trying fix basepath

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,10 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen">
-      <Image src='/huki-logo-transparent.png' alt='logo' width={450} height={450} className='mb-1'/>
+      <Image src='./huki-logo-transparent.png' alt='logo' width={450} height={450} className='mb-1'/>
       <div className='flex space-x-4'>
         <Link href='https://t.me/huki_snipe_bot' target='_blank'>
-          <Image src='/icons8-telegram-app.svg' alt='Telegram' width={48} height={48} />
+          <Image src='./icons8-telegram-app.svg' alt='Telegram' width={48} height={48} />
         </Link>
       </div>
     </main>
