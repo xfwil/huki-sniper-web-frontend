9af79e518b81fd128cb423a2be7e7c0c417f8fb6
xfwil <tokinaa@sikuder.dev>
2025-12-10 00:27:36 +0800

    update domain

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,34 @@
-import Image from 'next/image'
-import Link from 'next/link'
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="bg-black flex flex-col items-center justify-center min-h-screen">
-      <Image src='./huki-logo-transparent.png' alt='logo' width={450} height={450} className='mb-1'/>
-      <div className='flex space-x-4'>
-        <Link href='https://t.me/huki_snipe_bot' target='_blank'>
-          <Image src='./icons8-telegram-app.svg' alt='Telegram' width={48} height={48} />
+      <Image
+        src="./huki-logo-transparent.png"
+        alt="logo"
+        width={450}
+        height={450}
+        className="mb-1"
+      />
+      <div className="flex space-x-4">
+        <Link href="https://t.me/huki_snipe_bot" target="_blank">
+          <Image
+            src="./icons8-telegram-app.svg"
+            alt="Telegram"
+            width={48}
+            height={48}
+          />
+        </Link>
+        <Link href="https://discord.gg/huki" target="_blank">
+          <Image
+            src="./icons8-discord.svg"
+            alt="Discord"
+            width={48}
+            height={48}
+          />
         </Link>
       </div>
     </main>
-  )
+  );
 }
